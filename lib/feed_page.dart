218012e9e0e5489f66e5fdc53e33c7e5e1310d10
teo-KIM <ted243@naver.com>
2021-02-13 15:18:26 +0900

    [ADD] listview 생성

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -4,9 +4,23 @@ class FeedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       title: Text("instagram", style: TextStyle(fontFamily: "VeganStyle"),),
-     ),
+      appBar: AppBar(
+        title: Text(
+          "instagram",
+          style: TextStyle(fontFamily: "VeganStyle"),
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
+        itemCount: 40,
+      ),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Container(
+      height: 100,
+      color: Colors.accents[index % Colors.accents.length],
     );
   }
 }
