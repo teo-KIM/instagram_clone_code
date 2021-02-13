32c050579468d6f3f161bd1ee5eb392ed8df18c4
teo-KIM <ted243@naver.com>
2021-02-13 15:15:47 +0900

    [UPDATE] primary color 흰색으로 변경 // MaterialColor 파일 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_code/constants/material_white.dart';
 
 import 'home_page.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      theme: ThemeData(primarySwatch: white),
     );
   }
 }
