8f0d85324686aa69681745bf5be8487a211c2bea
teo-KIM <ted243@naver.com>
2021-02-13 16:16:15 +0900

    [ADD] network image 추가 / indicator 추가

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,17 +1,39 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
+import 'package:instagram_clone_code/widgets/my_progress_indicator.dart';
 
 class Post extends StatelessWidget {
   final int index;
-  const Post({
-    Key key, this.index,
+  Size _size;
+
+  Post({
+    Key key,
+    this.index,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      color: Colors.accents[index % Colors.accents.length],
+    _size = MediaQuery.of(context).size;
+    return CachedNetworkImage(
+      imageUrl: "https://picsum.photos/id/$index/200/200",
+      placeholder: (
+        BuildContext context,
+        String url,
+      ) {
+        return MyProgressIndicator(
+          containerSize: _size.width,
+        );
+      },
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                image:
+                    DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+          ),
+        );
+      },
     );
   }
 }
