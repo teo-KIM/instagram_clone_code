88ed4271ae9edcaaf478a1f32cfb469b4a3a7567
teo-KIM <ted243@naver.com>
2021-02-14 18:50:11 +0900

    [ADD] post header 생성

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_code/constants/common_size.dart';
 import 'package:instagram_clone_code/widgets/my_progress_indicator.dart';
 
 class Post extends StatelessWidget {
@@ -14,6 +15,33 @@ class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        _postHeader(),
+        _postImage(),
+      ],
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: ClipOval(
+              child: CachedNetworkImage(
+            imageUrl: 'https://picsum.photos/100',
+            width: avatar_size,
+            height: avatar_size,
+          )),
+        ),
+        Expanded(child: Text('username')),
+        IconButton(icon: Icon(Icons.more_horiz), onPressed: null)
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
     return CachedNetworkImage(
       imageUrl: "https://picsum.photos/id/$index/200/200",
       placeholder: (
