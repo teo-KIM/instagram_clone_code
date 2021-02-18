b32f2cde77ec3edc10f2cfd6f0492329adb000b8
teo-KIM <ted243@naver.com>
2021-02-18 15:08:16 +0900

    [UPDATE] editprofile 버튼 하단 tap 아이콘 버튼 및 기능 추가

--- lib/widgets/profile_body.dart
+++ lib/widgets/profile_body.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_code/constants/common_size.dart';
 
-class ProfileBody extends StatelessWidget {
+class ProfileBody extends StatefulWidget {
+  @override
+  _ProfileBodyState createState() => _ProfileBodyState();
+}
+
+class _ProfileBodyState extends State<ProfileBody> {
+  bool selectedLeft = true;
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: CustomScrollView(
         slivers: [
           SliverList(
-              delegate: SliverChildListDelegate(
-                  <Widget>[_username(), _userbio(), _editProfileBtn()]))
+              delegate: SliverChildListDelegate(<Widget>[
+            _username(),
+            _userbio(),
+            _editProfileBtn(),
+            Row(
+              children: [
+                Expanded(
+                  child: IconButton(
+                      icon: ImageIcon(
+                        AssetImage('assets/images/grid.png'),
+                        color: selectedLeft ? Colors.black : Colors.black26,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          selectedLeft = true;
+                        });
+                      }),
+                ),
+                Expanded(
+                  child: IconButton(
+                      icon: ImageIcon(
+                        AssetImage('assets/images/saved.png'),
+                        color: !selectedLeft ? Colors.black : Colors.black26,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          selectedLeft = false;
+                        });
+                      }),
+                )
+              ],
+            )
+          ]))
         ],
       ),
     );
@@ -18,20 +56,20 @@ class ProfileBody extends StatelessWidget {
 
 Padding _editProfileBtn() {
   return Padding(
-    padding: EdgeInsets.symmetric(horizontal: common_gap),
+    padding:
+        EdgeInsets.symmetric(horizontal: common_gap, vertical: common_xxs_gap),
     child: SizedBox(
       height: 24,
       child: OutlinedButton(
         onPressed: () {},
         child: Text(
           'Edit profile',
-          style:
-          TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
         ),
         style: OutlinedButton.styleFrom(
             side: BorderSide(color: Colors.black45),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(6))),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(6))),
       ),
     ),
   );
