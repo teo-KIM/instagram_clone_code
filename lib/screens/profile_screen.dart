fe34989f0a4e561493685db591ada1a77a61900b
teo <laon-public@>
2021-02-20 11:07:55 +0900

    [UPDATE] 프로필 사이드 메뉴 위젯 적용 및 position 삭제

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone_code/constants/common_size.dart';
 import 'package:instagram_clone_code/constants/screen_size.dart';
 import 'package:instagram_clone_code/widgets/profile_body.dart';
+import 'package:instagram_clone_code/widgets/profile_side_menu.dart';
+
+final duration = Duration(milliseconds: 1000);
 
 class ProfileScreen extends StatefulWidget {
-
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  final duration = Duration(milliseconds: 300);
+
   final menuWidth = size.width/2;
 
   MenuStatus _menuStatus = MenuStatus.CLOSED;
@@ -55,13 +57,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             transform: Matrix4.translationValues(menuXPos, 0, 0),
             duration: duration,
             curve: Curves.fastOutSlowIn,
-            child: Positioned(
-                top: 0,
-                bottom: 0,
-                width: size.width / 2,
-                child: Container(
-                  color: Colors.purpleAccent,
-                )),
+            child: ProfileSideMenu(menuWidth),
           )
         ],
       ),
