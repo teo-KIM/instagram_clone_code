87d87287e24dc5ad4eac00eafd67dc896396d4de
teo <laon-public@>
2021-02-27 11:14:09 +0900

    [UPDATE] 로그인 / 회원가입 페이지 변환 애니메이션 추가

---
+++ lib/widgets/fade_stack.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_code/widgets/sign_in_form.dart';
+import 'package:instagram_clone_code/widgets/sign_up_form.dart';
+
+class FadeStack extends StatefulWidget {
+  final int selectedForm;
+
+  const FadeStack({Key key, this.selectedForm}) : super(key: key);
+
+  @override
+  _FadeStackState createState() => _FadeStackState();
+}
+
+class _FadeStackState extends State<FadeStack>
+    with SingleTickerProviderStateMixin {
+  List<Widget> forms = [SignUpForm(), SignInForm()];
+
+  //FadeTransition을 사용하기 위함
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    //처음에 forward를 한번 해줘야 적용됨
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant FadeStack oldWidget) {
+    //oldWidget을 사용해서 이전 이전 위젯과 현재 위젯을 비교함
+    //widget.selectedForm = 최상단 Widget에 있는 변수를 사용할 때 widget.으로 사용한다
+    if (widget.selectedForm != oldWidget.selectedForm) {
+      _animationController.forward(from: 0.0);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animationController,
+      child: IndexedStack(
+        index: widget.selectedForm,
+        children: forms,
+      ),
+    );
+  }
+}
