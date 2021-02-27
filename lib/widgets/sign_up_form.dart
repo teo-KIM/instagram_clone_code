6a8c54ffd6657a71bb369b0d0f6c5e5973f3d791
teo <laon-public@>
2021-02-27 10:56:54 +0900

    [UPDATE] 로그인 / 회원가입 페이지 구분을 위한 color 추가

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class SignUpForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.greenAccent,
+    );
   }
 }
