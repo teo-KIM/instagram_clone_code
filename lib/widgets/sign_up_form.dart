eba0e0c7c4778c4b1e36327582341f4015f4040d
teo <laon-public@>
2021-03-08 22:25:18 +0900

    [UPDATE] common_size.dart gap large 사이즈 추가, 회원가입 페이지 layout 생성중. 상단 TextImage 및 TextFormField 추가

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_code/constants/common_size.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+
+  //form의 상태를 가지고 있는 key
+  //현재 그 안에 어떤 데이터를 가지고있는지(Text)
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
-    );
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: [
+          SizedBox(width: common_l_gap,),
+          Image.asset('assets/images/insta_text_logo.png'),
+          TextFormField(
+
+          ),
+          TextFormField(
+
+          ),
+          TextFormField(
+
+          ),
+        ],
+      ),);
   }
 }
